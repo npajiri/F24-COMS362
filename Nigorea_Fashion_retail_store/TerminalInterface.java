83f22a41ad8ef6c0e326ae141109c80ca3bf21c1
Ajiri <npajiri@iastate.edu>
2024-12-09 12:27:00 -0600

    updated exiting to main menu logic

--- Nigorea_Fashion_retail_store/TerminalInterface.java
+++ Nigorea_Fashion_retail_store/TerminalInterface.java
@@ -4,52 +4,58 @@ public class TerminalInterface {
     public void start() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("=== Welcome to the Nigorea Fashion Store ===");
-        System.out.println("Select your role:");
-        System.out.println("1. Customer");
-        System.out.println("2. Supplier");
-        System.out.println("3. Admin");
-        System.out.println("4. Employee");
-        System.out.println("5. Customer Service Agent");
-        System.out.println("6. Exit Store");
-        System.out.print("Enter your choice: ");
+        boolean running = true;
 
-        int roleChoice = scanner.nextInt();
-        scanner.nextLine(); // Consume newline
+        while (running) {
 
-        switch (roleChoice) {
-            case 1:
-                CustomerMenu customerMenu = new CustomerMenu();
-                customerMenu.showMenu(scanner);
-                break;
+            System.out.println("=== Welcome to the Nigorea Fashion Store ===");
+            System.out.println("Select your role:");
+            System.out.println("1. Customer");
+            System.out.println("2. Supplier");
+            System.out.println("3. Admin");
+            System.out.println("4. Employee");
+            System.out.println("5. Customer Service Agent");
+            System.out.println("6. Exit Store");
+            System.out.print("Enter your choice: ");
 
-            case 2:
-                SupplierMenu supplierMenu = new SupplierMenu();
-                supplierMenu.showMenu(scanner);
-                break;
+            int roleChoice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
 
-            case 3:
-                AdminMenu adminMenu = new AdminMenu();
-                adminMenu.showMenu(scanner);
-                break;
+            switch (roleChoice) {
+                case 1:
+                    CustomerMenu customerMenu = new CustomerMenu();
+                    customerMenu.showMenu(scanner);
+                    break;
 
-            case 4:
-                EmployeeMenu employeeMenu = new EmployeeMenu();
-                employeeMenu.showMenu(scanner);
-                break;
+                case 2:
+                    SupplierMenu supplierMenu = new SupplierMenu();
+                    supplierMenu.showMenu(scanner);
+                    break;
 
-            case 5:
-                System.out.println("Customer Service Agent options are not yet implemented.");
-                break;
+                case 3:
+                    AdminMenu adminMenu = new AdminMenu();
+                    adminMenu.showMenu(scanner);
+                    break;
 
-            case 6:
-                System.out.println("Thanks for stopping by! We hope to see you again.....");
-                break;
+                case 4:
+                    EmployeeMenu employeeMenu = new EmployeeMenu();
+                    employeeMenu.showMenu(scanner);
+                    break;
 
-            default:
-                System.out.println("Invalid choice. Exiting.");
+                case 5:
+                    System.out.println("Customer Service Agent options are not yet implemented.");
+                    break;
+
+                case 6:
+                    System.out.println("Thanks for stopping by! We hope to see you again.....");
+                    running = false;
+                    break;
+
+                default:
+                    System.out.println("Invalid choice. Exiting.");
+            }
         }
-
         scanner.close();
+    
     }
 }
